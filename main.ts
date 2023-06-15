f19e13e1471384145880c938db2adc2d2f7ff3e6
Abhinav Khemka <akhemka@live.com>
2023-06-15 11:07:33 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(5)
+    basic.showIcon(IconNames.No)
+})
+radio.setGroup(81)
+music.playMelody("F A F A F A F A ", 500)
 basic.forever(function () {
-	
+    if (input.rotation(Rotation.Roll) < -45) {
+        radio.sendNumber(3)
+        basic.showIcon(IconNames.Heart)
+    } else if (input.rotation(Rotation.Roll) > 45) {
+        radio.sendNumber(4)
+        basic.showIcon(IconNames.SmallHeart)
+    } else if (input.rotation(Rotation.Pitch) < -45) {
+        radio.sendNumber(1)
+        basic.showIcon(IconNames.Tortoise)
+    } else if (input.rotation(Rotation.Pitch) > 45) {
+        radio.sendNumber(2)
+        basic.showIcon(IconNames.StickFigure)
+    } else {
+        radio.sendNumber(0)
+        basic.showIcon(IconNames.Square)
+    }
 })
